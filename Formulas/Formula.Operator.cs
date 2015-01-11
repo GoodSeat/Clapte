e089e83ed2d091cf53d30521ae73f1fffc65c801
GoodSeat <GoodSeat@hotmail.co.jp>
2015-01-12 00:34:15 +0900

    数式演算で、1/xの結果は、1*x^-1 ではなく x^-1 とするようにした。

--- Formulas/Formula.Operator.cs
+++ Formulas/Formula.Operator.cs
@@ -11,24 +11,24 @@ namespace GoodSeat.Liffom.Formulas
 		/// <summary>
 		/// 加算を生成します。
 		/// </summary>
-		/// <param name="f1">数式1</param>
-		/// <param name="f2">数式1</param>
-		/// <returns>加算</returns>
+		/// <param name="f1">数式1。</param>
+		/// <param name="f2">数式1。</param>
+		/// <returns>加算。</returns>
 		public static Sum operator +(Formula f1, Formula f2) { return new Sum(f1, f2); }
 
 		/// <summary>
 		/// 減算（加算）を生成します。
 		/// </summary>
-		/// <param name="f1">数式1</param>
-		/// <param name="f2">数式1</param>
-		/// <returns>減算</returns>
+		/// <param name="f1">数式1。</param>
+		/// <param name="f2">数式1。</param>
+		/// <returns>減算。</returns>
 		public static Sum operator -(Formula f1, Formula f2) { return new Sum(f1, -f2); }
 
 		/// <summary>
 		/// 負数を生成します。
 		/// </summary>
-		/// <param name="f1">数式</param>
-		/// <returns>負数</returns>
+		/// <param name="f1">数式。</param>
+		/// <returns>負数。</returns>
 		public static Formula operator -(Formula f1) 
 		{
 			var num = f1 as Numeric;
@@ -42,30 +42,32 @@ namespace GoodSeat.Liffom.Formulas
 		/// <summary>
 		/// 乗算(<paramref name="f1"/>^<paramref name="f2"/>)を生成します。
 		/// </summary>
-		/// <param name="f1">数式1</param>
-		/// <param name="f2">数式2</param>
-		/// <returns>乗算</returns>
+		/// <param name="f1">数式1。</param>
+		/// <param name="f2">数式2。</param>
+		/// <returns>乗算。</returns>
 		public static Product operator *(Formula f1, Formula f2) { return new Product(f1, f2); }
 
 		/// <summary>
 		/// 除算を生成します。
 		/// </summary>
-		/// <param name="f1">分子</param>
-		/// <param name="f2">分母</param>
-		/// <returns>除算</returns>
-		public static Product operator /(Formula f1, Formula f2)
+		/// <param name="f1">分子。</param>
+		/// <param name="f2">分母。</param>
+		/// <returns>除算。</returns> return divide;
+		public static Formula operator /(Formula f1, Formula f2)
 		{
 			Power divide = f2 ^ -1;
 			divide.Format.SetProperty(new DivisionFormatProperty(true));
+
+			if (f1 is Numeric && (f1 == 1)) return divide;
 			return f1 * divide;
 		}
 
 		/// <summary>
 		/// 累乗を生成します。
 		/// </summary>
-		/// <param name="f1">基数</param>
-		/// <param name="f2">指数</param>
-		/// <returns>累乗</returns>
+		/// <param name="f1">基数。</param>
+		/// <param name="f2">指数。</param>
+		/// <returns>累乗。</returns>
 		public static Power operator ^(Formula f1, Formula f2)
 		{
 			Power power = new Power(f1, f2);
@@ -80,10 +82,10 @@ namespace GoodSeat.Liffom.Formulas
 		/// 参照の等価判定を行うには、object.ReferenceEqualsメソッドを用いてください。
 		/// 値の順序や、括弧の種類の違いは等価判定に影響しません。
 		/// </remarks>
-		/// <example>2+4 == 4+2ではtrue、4/2 == 2ではfalse</example>
-		/// <param name="f1">数式1</param>
-		/// <param name="f2">数式2</param>
-		/// <returns>比較結果</returns>
+		/// <example>2+4 == 4+2ではtrue、4/2 == 2ではfalse。</example>
+		/// <param name="f1">数式1。</param>
+		/// <param name="f2">数式2。</param>
+		/// <returns>比較結果。</returns>
 		public static bool operator ==(Formula f1, Formula f2)
 		{
 			// nullチェック
@@ -111,10 +113,10 @@ namespace GoodSeat.Liffom.Formulas
 		/// <summary>
 		/// 2数式の子数式の数が等しいか否かを取得します。
 		/// </summary>
-		/// <param name="f1">数式1</param>
-		/// <param name="f2">数式2</param>
-		/// <param name="count">子数式数のうち小さい方</param>
-		/// <returns>子数式の数が等しい場合true</returns>
+		/// <param name="f1">数式1。</param>
+		/// <param name="f2">数式2。</param>
+		/// <param name="count">子数式数のうち小さい方。</param>
+		/// <returns>子数式の数が等しい場合true。</returns>
 		private static bool CheckSameChildCount(Formula f1, Formula f2, out int count)
 		{
 			count = 0;
@@ -129,9 +131,9 @@ namespace GoodSeat.Liffom.Formulas
 		/// Formulaでは、Equalsメソッドと==演算子で、いずれも値の等価判定が行われます。
 		/// 参照の等価判定を行うには、object.ReferenceEqualsメソッドを用いてください。
 		/// </remarks>
-		/// <param name="f1">数式1</param>
-		/// <param name="f2">数式2</param>
-		/// <returns>比較結果</returns>
+		/// <param name="f1">数式1。</param>
+		/// <param name="f2">数式2。</param>
+		/// <returns>比較結果。</returns>
 		public static bool operator !=(Formula f1, Formula f2) { return !(f1 == f2); }
 
 		/// <summary>
@@ -141,8 +143,8 @@ namespace GoodSeat.Liffom.Formulas
 		/// Formulaでは、Equalsメソッドと==演算子で、いずれも値の等価判定が行われます。
 		/// 参照の等価判定を行うには、object.ReferenceEqualsメソッドを用いてください。
 		/// </remarks>
-		/// <param name="f">比較対象の数式</param>
-		/// <returns>比較結果</returns>
+		/// <param name="f">比較対象の数式。</param>
+		/// <returns>比較結果。</returns>
 		public bool Equals(Formula f) { return this == f; }
 
 		/// <summary>
@@ -152,8 +154,8 @@ namespace GoodSeat.Liffom.Formulas
 		/// Formulaでは、Equalsメソッドと==演算子で、いずれも値の等価判定が行われます。
 		/// 参照の等価判定を行うには、object.ReferenceEqualsメソッドを用いてください。
 		/// </remarks>
-		/// <param name="f">比較対象の数式</param>
-		/// <returns>比較結果</returns>
+		/// <param name="f">比較対象の数式。</param>
+		/// <returns>比較結果。</returns>
 		public override bool Equals(object obj)
 		{
 			if (obj is Formula)
@@ -171,7 +173,7 @@ namespace GoodSeat.Liffom.Formulas
 		/// <summary>
 		/// Double型への暗黙的変換（変換できない場合、double.NaNを返します）
 		/// </summary>
-		/// <param name="f">対象の数式</param>
+		/// <param name="f">対象の数式。</param>
 		/// <returns></returns>
 		public static implicit operator double(Formula f)
 		{
