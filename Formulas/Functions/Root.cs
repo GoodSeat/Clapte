7e3203ac1ed245beb5720a70c1f0db5cc0c317aa
GoodSeat <GoodSeat@hotmail.co.jp>
2015-01-11 18:16:41 +0900

    速度向上のため、Rootの計算結果の表現を修正した。

--- Formulas/Functions/Root.cs
+++ Formulas/Functions/Root.cs
@@ -47,9 +47,9 @@ namespace GoodSeat.Liffom.Formulas.Functions
 		public override Formula CalculateFunction()
         {
 			if (Argument.Count < 2 || this[1] is Null)
-				return new Power(this[0], 1 / new Numeric(2));
+				return new Power(this[0], new Numeric(2) ^ 1);
 			else
-				return new Power(this[0], 1 / this[1]);
+				return new Power(this[0], this[1] ^ 1);
         }
 
 		public override int MinimumArgumentQty { get { return 1; } }
