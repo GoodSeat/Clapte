075cb339708d338e32fb8954ce5d445edaa47974
GoodSeat <GoodSeat@hotmail.co.jp>
2015-01-18 19:28:29 +0900

    Squashed 'Sio/' content from commit 80a898b
    git-subtree-dir: Sio
    git-subtree-split: 80a898bf79dd4850bbfcdd155f1d1c6bc057f5f0

---
+++ Xml/XmlAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Adaptee = System.Xml;
+
+namespace GoodSeat.Sio.Xml
+{
+	/// <summary>
+	/// XmlElementの属性を表します。
+	/// </summary>
+	public class XmlAttribute
+	{
+		string _attributeName = "";
+		string _attributeValue = "";
+
+		/// <summary>
+		/// XmlElementの属性を初期化します。
+		/// </summary>
+		/// <param name="name">属性の名称</param>
+		/// <param name="value">属性の値</param>
+		public XmlAttribute(string name, string value)
+		{
+			_attributeName = name;
+			_attributeValue = value;
+		}
+
+		/// <summary>
+		/// 属性の名称を設定もしくは取得します。
+		/// </summary>
+		public string Name { get { return _attributeName; } set { _attributeName = value; } }
+
+		/// <summary>
+		/// 属性の値を設定もしくは取得します。
+		/// </summary>
+		public string Value { get { return _attributeValue; } set { _attributeValue = value; } }
+
+	}
+}
