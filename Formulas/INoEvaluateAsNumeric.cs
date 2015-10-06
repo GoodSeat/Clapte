87f4dc032e1232293a70189091d8f6573b9e3e78
GoodSeat <GoodSeat@hotmail.co.jp>
2015-10-06 17:46:12 +0900

    単位など、数値化されることのない性質を表すインターフェイスを追加した。

---
+++ Formulas/INoEvaluateAsNumeric.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoodSeat.Liffom.Formulas
+{
+    /// <summary>
+    /// 単位など、数値として評価されえることのない数式を表します。
+    /// </summary>
+    public interface INoEvaluateAsNumeric
+    {
+    }
+}
