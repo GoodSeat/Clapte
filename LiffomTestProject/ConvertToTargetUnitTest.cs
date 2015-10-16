dec05f5044b459587e2b0e9f824dcddc0b3fda22
GoodSeat <GoodSeat@hotmail.co.jp>
2015-10-16 21:51:36 +0900

    数式置き換え時に、書式情報を置き換えるか否かを引数で指定できるようにした。

--- LiffomTestProject/ConvertToTargetUnitTest.cs
+++ LiffomTestProject/ConvertToTargetUnitTest.cs
@@ -75,7 +75,7 @@ namespace GoodSeat.LiffomTestProject
 		{
 			UnitConvertTableTest.RegistBasicUnitRecord();
 
-			ConvertToTargetUnit target = new ConvertToTargetUnit(Formula.SimplifyToken);
+			ConvertToTargetUnit target = new ConvertToTargetUnit();
 			var test = Formula.Parse("5[kgf] + 5[N]");
 			var targetUnit = new Unit("kN");
 			var actual = target.Do(test, targetUnit).Numerate();
