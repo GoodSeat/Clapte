1ad36f63c40bba7b4fd6d6a748eeb383698df947
GoodSeat <GoodSeat@hotmail.co.jp>
2016-05-27 19:23:43 +0900

    数式によっては、異常なまでに計算速度が遅くなるので、piやeは早い段階で数値化してしまうことにした。

--- Clapte/ViewModels/SolverViewModel.cs
+++ Clapte/ViewModels/SolverViewModel.cs
@@ -281,11 +281,11 @@ namespace GoodSeat.Clapte.ViewModels
             else
             {
                 // tan(pi/2)やe^ix 等を正しくルール変形するため、piやe等の定数と、累乗の数値化は後回しにする。
-                var token1 = new DeformToken(new SimplifyToken(), new NumerateToken(), new CalculateToken());
-                token1.NoTryRules.Add(CalculatePowerNumericRule.Entity); // 累乗の数値化を除外
-                token1.NoTryRules.Add(ConstantNumerateRule.Entity); // 定数の数値化を除外
-                token1.NoTryRules.Add(CalculateDivideNumericRule.Entity); // 数値の除算を除外
-                tokenList.Add(token1);
+//                var token1 = new DeformToken(new SimplifyToken(), new NumerateToken(), new CalculateToken());
+//                token1.NoTryRules.Add(CalculatePowerNumericRule.Entity); // 累乗の数値化を除外
+//                token1.NoTryRules.Add(ConstantNumerateRule.Entity); // 定数の数値化を除外
+//                token1.NoTryRules.Add(CalculateDivideNumericRule.Entity); // 数値の除算を除外
+//                tokenList.Add(token1);
 
                 // その後、小数まで計算しきる。
                 var token2 = new DeformToken(new SimplifyToken(), new NumerateToken(), new CalculateToken());
