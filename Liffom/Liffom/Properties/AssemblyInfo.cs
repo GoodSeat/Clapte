0e77fcc020f26044131a671fa7d5b620115f0fed
GoodSeat <GoodSeat@hotmail.co.jp>
2016-12-18 22:48:39 +0900

    バージョン情報更新(Ver2.0.0 R20161218)

--- Liffom/Liffom/Properties/AssemblyInfo.cs
+++ Liffom/Liffom/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GoodSeat || BadSeat")]
 [assembly: AssemblyProduct("Liffom")]
-[assembly: AssemblyCopyright("Copyright © 2012 いいせき All Rights Reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2016 いいせき All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("LiffomTestProject")]
