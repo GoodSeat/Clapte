9a0eaa85aab2cf985b0ce577c829bafe19f50f8a
GoodSeat <GoodSeat@hotmail.co.jp>
2018-10-29 20:50:56 +0900

    resolve #29

--- Liffom/LiffomTestProject/CalculateUnitConversionFactorTest.cs
+++ Liffom/LiffomTestProject/CalculateUnitConversionFactorTest.cs
@@ -106,6 +106,7 @@ namespace GoodSeat.LiffomTestProject
             AddTestCase("[atm*l*mol/J]", "101.325[mol]");
             AddTestCase("[atm*l]", "[J]", "101.325");
             AddTestCase("[rad]", "1");
+            AddTestCase("[kg]", "[kg*cm/m]", "100");
         }
 
         private void AddTestCase(string from, string to, string expectedConversionFactor)
