c9eed671f4eaf454fccf768c8291aa355a0625e1
GoodSeat <GoodSeat@hotmail.co.jp>
2020-03-29 22:32:46 +0900

    バージョン情報の更新

--- Clapte/Views/Forms/SettingPanels/VersionInformationPanel.cs
+++ Clapte/Views/Forms/SettingPanels/VersionInformationPanel.cs
@@ -127,7 +127,7 @@ namespace GoodSeat.Clapte.Views.Forms.SettingPanels
         {
             get
             {
-                return "20190920";
+                return "20200329";
             }
         }
 
